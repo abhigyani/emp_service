a45251b88fe5e84649635744cea75f5371499cab
Abhigyani Anand <abhigyani@Abhigyanis-MacBook-Pro.local>
2024-05-17 23:34:33 +0530

    Removed unwanted print statement

--- app/database.py
+++ app/database.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from settings import get_settings
 
 SQLALCHEMY_DATABASE_URL = get_settings().MYSQL_DB_URL
-print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(url=SQLALCHEMY_DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
